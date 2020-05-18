6142536484a1c3919bf0b7367bfab231733b17cb
Gowtham-v-ui <gowtham.velmurugan@vivriticapital.com>
2020-05-18 21:41:07 +0530

    Updates

---
+++ precache-manifest.4c581c63c6656ade40c18398fb6434e2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c68cfa35ca9c7110a25d4cd9279a72",
+    "url": "/Adobe/index.html"
+  },
+  {
+    "revision": "2a7dcc6980711ed6d05d",
+    "url": "/Adobe/static/css/2.60eec0d8.chunk.css"
+  },
+  {
+    "revision": "6a59c37cf7eef8545277",
+    "url": "/Adobe/static/css/main.8818c29c.chunk.css"
+  },
+  {
+    "revision": "2a7dcc6980711ed6d05d",
+    "url": "/Adobe/static/js/2.e35e7cfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Adobe/static/js/2.e35e7cfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a59c37cf7eef8545277",
+    "url": "/Adobe/static/js/main.7f5cf38b.chunk.js"
+  },
+  {
+    "revision": "151060078f5dab55cc0f",
+    "url": "/Adobe/static/js/runtime-main.82de47e8.js"
+  },
+  {
+    "revision": "316b15399d4ba1ec6ffd3421a011120c",
+    "url": "/Adobe/static/media/cart.316b1539.png"
+  },
+  {
+    "revision": "cbc92efe3d6feb2522e13e3d6cf4361d",
+    "url": "/Adobe/static/media/search.cbc92efe.png"
+  },
+  {
+    "revision": "37a3899264316247f894d733ef5976a9",
+    "url": "/Adobe/static/media/star.37a38992.png"
+  }
+]);
